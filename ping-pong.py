d6d40573fe29dd1cbd520a5ba756c36bf9066d7b
KoDixx <121109579+KoDixx@users.noreply.github.com>
2023-09-29 17:18:44 +0300

    Update ping-pong.py

--- ping-pong.py
+++ ping-pong.py
@@ -1,5 +1,29 @@
 from pygame import *
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, pl_img, x,y, speed):
+        super().__init__()
+        self.image = transform.scale(image.load(pi_img), (70, 70))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = speed
+
+class Player(GameSprite):
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 10:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < 600:
+            self.rect.y += self.speed  
+
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_UP] self.rect.y > 10:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < 600:
+            self.rect.y += self.speed  
+
 window = display.set_mode((700,500))
 display.set_caption('ping-pong')
 bacbackground = transform.scale(image.load('fon.jpg'),(700,500))
